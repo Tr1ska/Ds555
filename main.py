1cc8659c0425b550c4e5882b15a12955353a4a1f
Tr1ska <senya12200908@gmail.com>
2023-11-04 17:59:56 +0200

    Ds5

---
+++ main.py
@@ -0,0 +1,7 @@
+import colorama
+
+print("Назва: colorama - Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file")
+print("Вміст colorama: ansi, ansitowin32, ititialise, tests, win32, wintern")
+print("Дані: Back = <colorama.ansi.AnsiBack object>, Cursor = <colorama.ansi.AnsiCursor object>, Fore = <colorama.ansi.AnsiFore object>, "
+      "Style = <colorama.ansi.AnsiStyle object>")
+print("Версія: 0.4.6")
